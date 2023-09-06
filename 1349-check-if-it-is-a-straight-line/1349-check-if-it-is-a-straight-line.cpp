ed174bccf22559404ee83c9eceee45172707c8ef
Kanishk Bajpai <82649835+ikbajpai@users.noreply.github.com>
2023-09-06 14:32:24 +0530

    Time: 7 ms (78.42%), Space: 10 MB (68.74%) - LeetHub

--- 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.cpp
+++ 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    bool lineFunc(vector<vector<int>>& coordinates){
-    //    int m = /;
-        int a=0;
-        for(int i=2; i<coordinates.size();i++){
-            a=(coordinates[i][1]-coordinates[0][1])*(coordinates[1][0]-coordinates[0][0]) - ((coordinates[1][1] - coordinates[0][1]))*(coordinates[i][0]-coordinates[0][0]);
-            if(a){
+    // Helper function to check if a set of coordinates lies on the same line.
+    bool lineFunc(vector<vector<int>>& coordinates) {
+        int a = 0;
+        for (int i = 2; i < coordinates.size(); i++) {
+            a = (coordinates[i][1] - coordinates[0][1]) * (coordinates[1][0] - coordinates[0][0]) -
+                ((coordinates[1][1] - coordinates[0][1])) * (coordinates[i][0] - coordinates[0][0]);
+            if (a) {
                 return false;
             }
         }
         return true;
     }
+
+    // Main function to check if all coordinates form a straight line.
     bool checkStraightLine(vector<vector<int>>& coordinates) {
-        if(coordinates.size()<=2) return 1;
+        if (coordinates.size() <= 2) {
+            return true; // If there are 2 or fewer coordinates, they always form a straight line.
+        }
 
         return lineFunc(coordinates);
-
-
     }
 };
