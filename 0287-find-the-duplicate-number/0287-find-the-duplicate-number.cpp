ec066816f48645be73b4ff594190abe81030f1c2
Kanishk Bajpai <82649835+ikbajpai@users.noreply.github.com>
2023-09-11 22:36:40 +0530

    Time: 79 ms (80.28%), Space: 61.3 MB (48.98%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -8,10 +8,24 @@ public:
         // }
         // return sum-(n*(n+1)/2);
 
-        sort(nums.begin(),nums.end());
-        for (int i=0; i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]) return nums[i];
+        // sort(nums.begin(),nums.end());
+        // for (int i=0; i<nums.size()-1;i++){
+        //     if(nums[i]==nums[i+1]) return nums[i];
+        // }
+        // return -1;
+
+        int ans=-1;
+        for(int i=0;i<nums.size();i++){
+            int index = abs(nums[i]);
+
+            if(nums[index]<0){
+            ans = index;
+            break;
+            }
+
+            nums[index]*=-1;
         }
-        return -1;
+        return ans;
+
     }
 };
