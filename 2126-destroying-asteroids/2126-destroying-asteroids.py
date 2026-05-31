e234e5ca634d1e6cbfb727cbc16ca486bccb48ec
Kanishk Bajpai <82649835+ikbajpai@users.noreply.github.com>
2026-05-31 11:05:11 +0530

    Time: 94 ms (21.88%), Space: 33.8 MB (75.43%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.py
+++ 2126-destroying-asteroids/2126-destroying-asteroids.py
@@ -1,11 +1,10 @@
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
-        planet_mass = mass
         asteroids.sort()
 
         for i in range(len(asteroids)):
-            if planet_mass>=asteroids[i]:
-                planet_mass+=asteroids[i]
+            if mass>=asteroids[i]:
+                mass+=asteroids[i]
             else:
                 return False
         return True        
