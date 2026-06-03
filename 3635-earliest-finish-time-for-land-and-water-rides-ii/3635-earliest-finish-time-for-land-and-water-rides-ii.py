9667aef3aa6d6f3ad3784e908d3d430d73bf56ab
Kanishk Bajpai <82649835+ikbajpai@users.noreply.github.com>
2026-06-03 23:39:50 +0530

    Time: 654 ms (18.18%), Space: 35.4 MB (36.36%) - LeetHub

--- 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.py
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.py
@@ -1,64 +1,37 @@
 class Solution:
-    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
-        water = sorted(zip(waterStartTime, waterDuration))
-        ws = [x[0] for x in water]
-        wd = [x[1] for x in water]
-        m = len(water)
+    def solve(self, A_start, A_dur, B_start, B_dur):
+        idxs = sorted(range(len(B_start)), key=lambda i: B_start[i])
         
-        # suffix min of (start + duration)
+        bs = [B_start[i] for i in idxs]
+        bd = [B_dur[i] for i in idxs]
+        m = len(bs)
+        
         suffix = [0]*m
-        suffix[-1] = ws[-1] + wd[-1]
+        suffix[-1] = bs[-1] + bd[-1]
         for i in range(m-2, -1, -1):
-            suffix[i] = min(suffix[i+1], ws[i] + wd[i])
+            val = bs[i] + bd[i]
+            suffix[i] = val if val < suffix[i+1] else suffix[i+1]
         
-        # prefix min of duration
         prefix = [0]*m
-        prefix[0] = wd[0]
+        prefix[0] = bd[0]
         for i in range(1, m):
-            prefix[i] = min(prefix[i-1], wd[i])
+            prefix[i] = bd[i] if bd[i] < prefix[i-1] else prefix[i-1]
         
         ans = float('inf')
         
-        # Try Land → Water
-        for i in range(len(landStartTime)):
-            endLand = landStartTime[i] + landDuration[i]
+        for i in range(len(A_start)):
+            endA = A_start[i] + A_dur[i]
+            idx = bisect.bisect_left(bs, endA)
             
-            idx = bisect.bisect_left(ws, endLand)
-            
-            # Case: water starts after
             if idx < m:
                 ans = min(ans, suffix[idx])
-            
-            # Case: water already open
             if idx > 0:
-                ans = min(ans, endLand + prefix[idx-1])
-        
-        # 🔁 Repeat SAME logic swapping roles (Water → Land)
-        
-        land = sorted(zip(landStartTime, landDuration))
-        ls = [x[0] for x in land]
-        ld = [x[1] for x in land]
-        n = len(land)
-        
-        suffix = [0]*n
-        suffix[-1] = ls[-1] + ld[-1]
-        for i in range(n-2, -1, -1):
-            suffix[i] = min(suffix[i+1], ls[i] + ld[i])
-        
-        prefix = [0]*n
-        prefix[0] = ld[0]
-        for i in range(1, n):
-            prefix[i] = min(prefix[i-1], ld[i])
-        
-        for j in range(len(waterStartTime)):
-            endWater = waterStartTime[j] + waterDuration[j]
-            
-            idx = bisect.bisect_left(ls, endWater)
-            
-            if idx < n:
-                ans = min(ans, suffix[idx])
-            
-            if idx > 0:
-                ans = min(ans, endWater + prefix[idx-1])
+                ans = min(ans, endA + prefix[idx-1])
         
         return ans
+    
+    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
+        return min(
+            self.solve(landStartTime, landDuration, waterStartTime, waterDuration),
+            self.solve(waterStartTime, waterDuration, landStartTime, landDuration)
+        )
