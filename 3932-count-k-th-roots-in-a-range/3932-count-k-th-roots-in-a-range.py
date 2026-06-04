e3ac6e425be64c75ba939cc81915bf63787c553a
Kanishk Bajpai <82649835+ikbajpai@users.noreply.github.com>
2026-06-04 18:25:35 +0530

    Time: 0 ms (100%), Space: 19.2 MB (82.77%) - LeetHub

--- 3932-count-k-th-roots-in-a-range/3932-count-k-th-roots-in-a-range.py
+++ 3932-count-k-th-roots-in-a-range/3932-count-k-th-roots-in-a-range.py
@@ -1,14 +1,30 @@
+import math
 class Solution:
     def countKthRoots(self, l: int, r: int, k: int) -> int:
-        count=0
-        if k==1:
-            count =r-l+1
-            return count
-        for x in range(r+1):
-            p=x**k
-            if l<=p<=r:
-                count+=1
-            elif p>r:
-                break
-        return count
+        def floor(n):
+            low, high= 0,n+1
+            while(low+1<high):
+                mid= (low+high)//2
+                if(pow(mid,k)<=n):
+                    low= mid
+                else:
+                    high= mid
+            return low
+        if(l==0):
+            return floor(r)+1
+        return floor(r)-floor(l-1)
+
+# class Solution:
+#     def countKthRoots(self, l: int, r: int, k: int) -> int:
+#         count=0
+#         if k==1:
+#             count =r-l+1
+#             return count
+#         for x in range(r+1):
+#             p=x**k
+#             if l<=p<=r:
+#                 count+=1
+#             elif p>r:
+#                 break
+#         return count
         
