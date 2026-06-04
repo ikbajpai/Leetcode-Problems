31061b8ce3bdc31cc1404a8fefedc373d7901012
Kanishk Bajpai <82649835+ikbajpai@users.noreply.github.com>
2026-06-04 10:26:42 +0530

    Time: 442 ms (17.63%), Space: 19.1 MB (91.95%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.py
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.py
@@ -1,32 +1,19 @@
 class Solution:
-    def ListOfDigits(self, num):
-        ans=[]
-        while num:
-            ans.append(num%10)
-            num=num//10
-        ans=ans[::-1]
-        return ans
-    def NumberOfDigits(self, num):
-        count=0
-        while num>0:
-            num=num//10
-            count+=1
-        return count
     def getWaviness(self, num):
-        if self.NumberOfDigits(num)>=3:
-            digits = self.ListOfDigits(num)
-            peak, valley=0,0
-            for i in range(1, len(digits)-1):
-                if digits[i-1]<digits[i]>digits[i+1]:
-                    peak+=1
-                elif digits[i-1]>digits[i]<digits[i+1]:
-                    valley+=1
-                else:
-                    continue
-            return peak+valley
-        return 0
+        digits = list(map(int, str(num)))
+        
+        if len(digits) < 3:
+            return 0
+        
+        peak = valley = 0
+        
+        for i in range(1, len(digits)-1):
+            if digits[i-1] < digits[i] > digits[i+1]:
+                peak += 1
+            elif digits[i-1] > digits[i] < digits[i+1]:
+                valley += 1
+        
+        return peak + valley
+
     def totalWaviness(self, num1: int, num2: int) -> int:
-        Waviness=0
-        for i in range(num1, num2+1):
-            Waviness+=self.getWaviness(i)
-        return Waviness
+        return sum(self.getWaviness(i) for i in range(num1, num2+1))
