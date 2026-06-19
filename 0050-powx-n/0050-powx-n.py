3f22c56f4e5c87db03bb4112499fbf593a0a0d16
Kanishk Bajpai <82649835+ikbajpai@users.noreply.github.com>
2026-06-19 16:23:06 +0530

    Time: 0 ms (100%), Space: 19.2 MB (97.69%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,3 +1,17 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        # base case
+        if n==0:
+            return 1
+        if n<0:
+            return self.myPow(1/x, -n)
+
+        half = self.myPow(x, n//2)
+
+        # recursive case
+        if n%2:
+            return x*half*half
+        else:
+            return half * half
+
+        
